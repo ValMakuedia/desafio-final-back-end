bf58086c0814500fd5189ea6e7d10b8d53bd84b8
ValMakuedia <94015556+ValMakuedia@users.noreply.github.com>
2022-05-25 21:34:05 -0300

    Update user.js
    Correção do "bory" por "body"

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 
 const registerUser = async (req, res) => {
-    const { name, email, password } = req.bory;
+    const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
         return res.status(400).json("Campo obrigatório")
