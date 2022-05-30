6f133841bedf91b549f06670d44cc7965c1199e0
ValMakuedia <valmak21@hotmail.com>
2022-05-30 20:54:13 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(routes);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`listenin at ${port}`)
 });
