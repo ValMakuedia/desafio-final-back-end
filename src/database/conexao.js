f6c296bae1c69334af483eef06c05d46a6c13cc6
ValMakuedia <valmak21@hotmail.com>
2022-05-29 23:35:15 -0300

    configração banco de dados heroku

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,14 +1,22 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'familialima',
-        database: 'api-desafio'
-    
+        host: 'ec2-54-211-255-161.compute-1.amazonaws.com',
+        user: 'kiqncuapexzqle',
+        password: 'f1729c0c75d3a21171495d80384dc13de819bc46d108b2207fdb9db967ce46b7',
+        database: 'dmkbktnntfekl',
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 })
 
 
-
 module.exports = knex;
+
+
+//URI
+//postgres://kiqncuapexzqle:f1729c0c75d3a21171495d80384dc13de819bc46d108b2207fdb9db967ce46b7@ec2-54-211-255-161.compute-1.amazonaws.com:5432/dmkbktnntfekl
+//Heroku CLI
+//heroku pg:psql postgresql - aerodynamic - 76426 --app api - desafio5
--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ const app = express();
 app.use(express.json())
 app.use(routes);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
