091b2a7c1e4a7bc1d302f6e86aa5be9891c2fef0
ValMakuedia <valmak21@hotmail.com>
2022-05-30 00:17:44 -0300

    teste

--- src/middleware/verifyLogin.js
+++ src/middleware/verifyLogin.js
@@ -14,7 +14,7 @@ const verifyLogin = async (req, res, next) => {
     try {
         const token = authorization.replace('Bearer ', '').trim();
         const seila = jwt.verify(token, segredo);
-        console.log(seila)
+        return seila
         const queryId = await knex('users').where('id', id)
 
         if (!queryId) {
