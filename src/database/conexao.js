002921d588b0103af6c82450a403306a85aac052
ValMakuedia <valmak21@hotmail.com>
2022-05-30 17:10:26 -0300

    deploy

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'ec2-54-211-255-161.compute-1.amazonaws.com',
-        user: 'kiqncuapexzqle',
-        password: 'f1729c0c75d3a21171495d80384dc13de819bc46d108b2207fdb9db967ce46b7',
-        database: 'dmkbktnntfekl',
+        host: 'ec2-34-198-186-145.compute-1.amazonaws.com',
+        user: 'uxvjtuityiisfg',
+        password: '0945c036703605374fb619ff453e622021d77175dd6f237ea4151017a662fdfd',
+        database: 'd8qn9kfssudebi',
         port: 5432,
         ssl: {
             rejectUnauthorized: false
